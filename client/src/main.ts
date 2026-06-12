f6e72115f2a5a13e9f553d6faed3e7d5c77bd958
James Nash <james-server@notanemail.com>
2026-06-12 14:49:38 +0000

    initial commit

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,34 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import Home from './views/Home.vue'
+import NotFound from './views/NotFound.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: '/',
+    name: 'home-alt',
+    component: Home
+  },
+  {
+    name: 'home',
+    path: '/home',
+    component: Home
+  },
+  {
+    name: 'not-found',
+    path: '/:pathMatch(.*)*',
+    component: NotFound
+  },
+]
+
+export const router = createRouter({
+  history: createWebHistory('/app'),
+  routes,
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
