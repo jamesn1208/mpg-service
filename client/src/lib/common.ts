83f382b68c3fa959bed1bf48d2aa724ac27bd96a
James Nash <james-server@notanemail.com>
2026-06-14 14:01:13 +0000

    Made the front end prettier, defined plan for each page in HTML a comment

--- client/src/lib/common.ts
+++ client/src/lib/common.ts
@@ -9,38 +9,33 @@ export function sleep(s: number) : Promise<void> {
     return new Promise(resolve => setTimeout(resolve, s * 1000));
 }
 
-export function callAPI(path: string, method: string, payload: any = {}) : Promise<any> {
-    return fetch(`${window.location.origin}${path}`, {
-        method: method,
-        credentials: 'include',
-        body: JSON.stringify(payload),
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
+function extractFailReason(res: any) : string {
+    const data = res.json()
+    if (data.message && data.status === false) {
+        return data.message;
+    }
+    return `HTTP ${res.status}: ${res.statusText}`;
+}
+
+export async function callAPI(path: string, method: string, payload: any = {}): Promise<any> {
+    try {
+        const res = await fetch(`${window.location.origin}${path}`, {
+            method: method,
+            credentials: 'include',
+            body: JSON.stringify(payload),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        });
+        if (!res.ok) {
+            throw new Error(extractFailReason(res))
         }
-    })
-        .then(res => {
-            if (!res.ok) {
-                res.json().then(data => {
-                    if (data.message && data.status === false) {
-                        throw new Error(data.error_message)
-                    }
-                }).catch(_ => {
-                    throw new Error(`HTTP ${res.status}: ${res.statusText}`)
-                })
-            }
-            return res.json()
-        })
-        .then(data => {
-            if (data.message && data.status === false) {
-                throw new Error(data.error_message)
-            }
-            return data
-        })
-        .catch(reason => {
-            console.error(reason.toString())
-            throw new Error(reason)
-        })
+        return await res.json();
+    } catch (reason: any) {
+        console.error(reason.toString());
+        throw new Error(reason);
+    }
 }
 
 export function getFakeUsername() : string {
